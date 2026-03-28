b50be45a11fddccc4f0d1034eb5e3f9d511bedf6
HafizHuzaifa76 <hafizm.huzaifa1234gf@gmail.com>
2026-03-28 13:40:41 +0500

    palindrome problem soution without converting to string

--- number/palindrom_number/solution.py
+++ number/palindrom_number/solution.py
@@ -8,3 +8,22 @@ class Solution:
                 return True
             else:
                 return False
+
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            rev = x % 10
+            num = x / 10
+            while num > 1 :
+                last = int(num % 10)
+                rev = rev*10 + last
+                num = num/10
+
+            if x == rev:
+                return True
+            else:
+                return False
