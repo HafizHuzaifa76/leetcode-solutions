99c304a4282488f67cc251041ba302ba30b56b63
HafizHuzaifa76 <hafizm.huzaifa1234gf@gmail.com>
2026-05-01 01:22:17 +0500

    alternate solutuon of 3sun_closest with better approach

--- arrays/3sum_closest/solution.py
+++ arrays/3sum_closest/solution.py
@@ -17,3 +17,29 @@ class Solution:
                         final_sum = sum
         
         return final_sum
+
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        
+        final_sum = nums[0] + nums[1] + nums[2]
+        final_diff = abs(final_sum - target)
+        nums.sort()
+
+        for i in range(len(nums)):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                sum = nums[i] + nums[j] + nums[k]
+                diff = abs(sum - target)
+                if diff < final_diff :
+                    final_diff = diff
+                    final_sum = sum
+
+                if sum < target:
+                    j += 1
+                else:
+                    k -= 1
+        
+        return final_sum
